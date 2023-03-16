868042a40a9cbed72aca5e80f7844062dbd12601
fatmamontaser <89998228+fatmamontaser@users.noreply.github.com>
2023-03-16 21:31:37 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+    var typed = new Typed(".mov",{
+    strings: [, "Youtuber" ,"Developer","Freelancer","FrontEnd","Designer"] ,
+    typeSpeed: 150,
+    backSpeed: 100,
+    loop: true
+
+    })
+
+
+    console.log(typed)
